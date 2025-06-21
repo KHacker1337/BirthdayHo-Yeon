272544288f8a4d0bebac616d409ddd1e981d4aa4
Khacker <126566183+KHacker1337@users.noreply.github.com>
2025-06-22 00:11:43 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.querySelector('.container');
+    if (container) {
+        container.style.opacity = '1';
+    }
+});
